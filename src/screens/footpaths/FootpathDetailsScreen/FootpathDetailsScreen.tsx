5c93b273059723af3ab5f831c3bd85ea9c4a4528
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2023-01-05 10:44:09 +0100

    Placeholder details page

--- src/screens/footpaths/FootpathDetailsScreen/FootpathDetailsScreen.tsx
+++ src/screens/footpaths/FootpathDetailsScreen/FootpathDetailsScreen.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { FootpathDetailsScreenProps } from "../../../navigation/FootpathStack";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text } from "react-native-paper";
+import { Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const FootpathDetailsScreen = ({
 	navigation,
@@ -11,9 +14,53 @@ const FootpathDetailsScreen = ({
 	});
 
 	return (
-		<View>
-			<Text>FootpathDetailsScreen</Text>
-		</View>
+		<ScrollView>
+			<Text>{route.params.footpath.description}</Text>
+			<Image
+				source={{ uri: "https://picsum.photos/100/100" }}
+				style={{ height: 100, width: 100 }}
+			/>
+			<Image
+				source={{ uri: "https://picsum.photos/100/100" }}
+				style={{ height: 100, width: 100 }}
+			/>
+			<Image
+				source={{ uri: "https://picsum.photos/100/100" }}
+				style={{ height: 100, width: 100 }}
+			/>
+			<Image
+				source={{ uri: "https://picsum.photos/100/100" }}
+				style={{ height: 100, width: 100 }}
+			/>
+
+			<Text>Posizione QR code:</Text>
+
+			<MapView
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0322,
+					longitudeDelta: 0.0121,
+				}}
+				style={{
+					width: "100%",
+					height: 300,
+				}}
+			>
+				<Marker
+					title="inizio"
+					coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+				/>
+				<Marker
+					title="midpath"
+					coordinate={{ latitude: 37.784, longitude: -122.4324 }}
+				/>
+				<Marker
+					title="fine"
+					coordinate={{ latitude: 37.78, longitude: -122.4324 }}
+				/>
+			</MapView>
+		</ScrollView>
 	);
 };
 
