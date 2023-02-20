ed0dcf0eccf8aaa4e825f30e9ef66df2237ba5bf
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2023-02-20 10:43:34 +0100

    small footpath description rework

--- src/screens/footpaths/FootpathDetailsScreen/FootpathDetailsScreen.tsx
+++ src/screens/footpaths/FootpathDetailsScreen/FootpathDetailsScreen.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { FootpathDetailsScreenProps } from "../../../navigation/FootpathStack";
 import { ScrollView } from "react-native-gesture-handler";
 import { Text } from "react-native-paper";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import FootpathBarCodeScannerFAB from "../../../components/FootpathDetailsScreen/FootpathBarCodeScannerFAB/FootpathBarCodeScannerFAB";
+import styles from "./styles";
 
 const FootpathDetailsScreen = ({
 	navigation,
@@ -16,54 +17,41 @@ const FootpathDetailsScreen = ({
 
 	return (
 		<>
-			<MapView
-				initialRegion={{
-					latitude: 37.78825,
-					longitude: -122.4324,
-					latitudeDelta: 0.0322,
-					longitudeDelta: 0.0121,
-				}}
-				style={{
-					width: "100%",
-					height: 300,
-				}}
-			>
-				<Marker
-					title="inizio"
-					coordinate={{
+			<ScrollView>
+				<MapView
+					initialRegion={{
 						latitude: 37.78825,
 						longitude: -122.4324,
+						latitudeDelta: 0.0322,
+						longitudeDelta: 0.0121,
 					}}
-				/>
-				<Marker
-					title="midpath"
-					coordinate={{ latitude: 37.784, longitude: -122.4324 }}
-				/>
-				<Marker
-					title="fine"
-					coordinate={{ latitude: 37.78, longitude: -122.4324 }}
-				/>
-			</MapView>
-			<ScrollView>
-				<Text>{route.params.footpath.description}</Text>
-				<Image
-					source={{ uri: "https://picsum.photos/100/100" }}
-					style={{ height: 100, width: 100 }}
-				/>
-				<Image
-					source={{ uri: "https://picsum.photos/100/100" }}
-					style={{ height: 100, width: 100 }}
-				/>
-				<Image
-					source={{ uri: "https://picsum.photos/100/100" }}
-					style={{ height: 100, width: 100 }}
-				/>
-				<Image
-					source={{ uri: "https://picsum.photos/100/100" }}
-					style={{ height: 100, width: 100 }}
-				/>
+					style={{
+						width: "100%",
+						height: 300,
+					}}
+				>
+					<Marker
+						title="inizio"
+						coordinate={{
+							latitude: 37.78825,
+							longitude: -122.4324,
+						}}
+					/>
+					<Marker
+						title="midpath"
+						coordinate={{ latitude: 37.784, longitude: -122.4324 }}
+					/>
+					<Marker
+						title="fine"
+						coordinate={{ latitude: 37.78, longitude: -122.4324 }}
+					/>
+				</MapView>
+				<View style={styles.descriptionSection}>
+					<Text variant="headlineLarge">{route.params.footpath.name}</Text>
+					<Text variant="bodyLarge">{route.params.footpath.description}</Text>
+				</View>
 
-				<Text>Posizione QR code:</Text>
+				{/* <Text>Posizione QR code:</Text> */}
 			</ScrollView>
 			<FootpathBarCodeScannerFAB
 				onPress={() =>
